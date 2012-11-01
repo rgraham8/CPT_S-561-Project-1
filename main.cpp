7d7d1dea07a7c682374d78cf000fe149efd5c68f
Robert Graham <robertgraham1906@gmail.com>
2012-11-01 00:03:57 -0700

    Added issue command functionality

--- main.cpp
+++ main.cpp
@@ -27,60 +27,128 @@ enum STATE
 class ROB_Entry
 {
 	public:
-		bool m_busy;
-		INSTRUCTION m_instruction;
-		STATE m_state;
-		long* m_destination;
+		bool m_busy; // whether unit is busy
+		INSTRUCTION m_instruction; // instruction being issued/executed
+		STATE m_state; // the state of the unit
+		long* m_destination; // the destination register
 		unsigned int m_entry_number; // entry number
+		ROB_Entry* m_next; // next pointer for circular buffer
 		
 		///////////////////////////////////////////////////////////////////////
 		// Constructor
 		///////////////////////////////////////////////////////////////////////
-		ROB_Entry(void):
+		ROB_Entry(void):	
 			m_busy(false),
 			m_instruction(NA),
 			m_state(EMPTY),
-			m_destination(NULL)
+			m_destination(NULL),
+			m_next(NULL)
    		{
 			m_entry_number = ROB_Entry::s_next_id++;
 		}
 	
 	private:
-		ROB_Entry* next;
 		static unsigned int s_next_id; // used to increment each entry number
 };
 
 class ROB
 {
 	public:
+		///////////////////////////////////////////////////////////////////////
+		/// Constructor: Initializes the ROB circular buffer
+		///////////////////////////////////////////////////////////////////////
 		ROB(void)
 		{
-			m_start = &m_buffer[0];
-			m_end = m_start;
+			m_head = NULL;
+			ROB_Entry* last;
+			ROB_Entry* new_node;
+			m_rob_head = NULL;
+			
+			// Build doublely linked list of ROB Entries
+			for (unsigned int i=0; i<ROB_SIZE; i++)
+			{
+				// Allocate new entry
+				new_node = new ROB_Entry();
+				
+				if (m_rob_head == NULL)
+				{
+					// Insert head of list
+					m_rob_head = new_node;
+					last = new_node;
+				}
+				else
+				{
+					// Insert new entry
+					last->m_next = new_node;
+					last = new_node;
+					
+					// Point last entry to head
+					last->m_next = m_rob_head;
+				}
+				
+				// Verify correct entry numbers are stored
+				assert(last->m_entry_number == i);
+			}
+			
+			// Initialize the tail pointer (available ROB slot)
+			m_tail = m_rob_head;
 		}
 		
-		bool issue_instruction();
+		bool issue_instruction(INSTRUCTION issued_instruction)
+		{
+			bool issue_successful = false;
+			
+			// Check if ROB has a free entry
+			if (m_tail != m_head)
+			{
+				if (m_head == NULL)
+				{
+					// The ROB is empty
+					m_head = m_rob_head;
+				}
+				
+				// Initialize entry parameters
+				m_tail->m_instruction = issued_instruction;
+				m_tail->m_state = ISSUE;
+				m_tail->m_busy = true;
+				
+				// Point tail to next entry to be filled
+				m_tail = m_tail->m_next;
+				
+				issue_successful = true;
+			}
+			
+			return issue_successful;
+		}
+		
 		bool execute_instruction();
 		bool commit_instruction();
 		
 	private:
-		ROB_Entry* m_start;
-		ROB_Entry* m_end;
-		ROB_Entry m_buffer[ROB_SIZE];
+		ROB_Entry* m_rob_head; // head of ROB buffer
+		
+		// Commit stage increments this to remove instructions
+		// from the list (FULL when head == tail)
+		ROB_Entry* m_head;
+		
+		// Dispatch stage increments this to add instructions
+		// to the list
+		ROB_Entry* m_tail;
 };
 
-// Initialize the ROB Entry number 
+// Initialize the ROB Entry numbers
 unsigned int ROB_Entry::s_next_id = 0;
 
-
+///////////////////////////////////////////////////////////////////////////////
+/// Program entry point
+///
+/// @param[in] argc number command-line of args
+/// @param[in] argv array of command-line arguments
+///////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[])
 {
     // Initialize the ROB buffer
 	ROB reorder_buffer;
-	
-	ROB_Entry x = reorder_buffer.get_entry(2);
-	
-	std::cout<<x.m_entry_number<<std::endl;
 
 	return 0;
 }
